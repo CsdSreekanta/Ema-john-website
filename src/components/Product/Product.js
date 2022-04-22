bae234c798ebda451fb074e880c025902fdd9b04
unknown <sreekanta.csd@gmail.com>
2022-04-22 14:09:47 +0600

    cart updated

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,13 +6,20 @@ const Product = (props) => {
   const {img, name, price, seller, ratings} =props.product
     return (
         <div className='product'>
+          
             <img src={img} alt="" />
             <p className='product-name'>{name}</p>
-            <p>Price:${price}</p>
+            <p className='product-price'>Price:${price}</p>
             <p><small>Manufacturer:{seller}</small></p>
             <p><small>Rating:{ratings}</small></p>
-          
+           
+
+            <button className='button-cart'>
+                    <p>Add to Card</p>
+            </button>
         </div>
+        
+   
     );
 };
 
